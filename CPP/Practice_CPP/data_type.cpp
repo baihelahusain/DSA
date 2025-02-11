a445bb38ba5cc9b2b2616667fc977bc0e66b3563
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-11 22:13:55 +0530

    Add files via upload

---
+++ CPP/Practice_CPP/data_type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = -123;
+    cout<<a<<endl;
+    char b = 'H';
+    cout<<b<<endl;
+    float c = 23.0;
+    cout<<" My Age is:"<<c<<endl;
+    bool d = true;
+    cout<<d<<endl;
+    int size = sizeof(a);
+    cout<<size<<endl;
+    float bize = sizeof(c);
+    cout<<bize<<endl;
+    char bite = sizeof(b);
+    cout<<bite<<endl;
+};
+    
