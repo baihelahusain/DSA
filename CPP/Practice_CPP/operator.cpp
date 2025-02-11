79e9d534104ac51569aae80d13b00e05a2df1586
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-11 22:39:12 +0530

    Update operator.cpp

--- CPP/Practice_CPP/operator.cpp
+++ CPP/Practice_CPP/operator.cpp
@@ -7,9 +7,10 @@ int main(){
     cout<<c<<endl;
     cout<<b/a<<endl;
     cout<<a/b<<endl;
-    /*if we did not initially define the data type data type depends on the data type of dividend whatever the dividend the result will be of same data type*/
+    /*if we did not initially define the data type then the data type depends on the data type of dividend whatever the dividend the result will be the same data type*/
     cout<<2/5<<endl;
     cout<<2./5<<endl;
+    cout<<5/2.3<<endl;
     cout<<a*b<<endl;
     cout<<a%b<<endl;
 };
