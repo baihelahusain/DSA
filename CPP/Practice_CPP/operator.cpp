68391fac286cfbbb40ea271052003c29e4b2dcc4
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-11 22:35:50 +0530

    Update and rename t to operator.cpp

---
+++ CPP/Practice_CPP/operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 32;
+    float b = 23.3;
+    float c=a+b;
+    cout<<c<<endl;
+    cout<<b/a<<endl;
+    cout<<a/b<<endl;
+    /*if we did not initially define the data type data type depends on the data type of dividend whatever the dividend the result will be of same data type*/
+    cout<<2/5<<endl;
+    cout<<2./5<<endl;
+};
