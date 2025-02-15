f703a87e0788ad3ef4c3ec5b02e2d9bc0f14f327
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-15 20:58:22 +0530

    Add files via upload

---
+++ CPP/Patterns/abc_box2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i =1;
+    cin>>n;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            char a = 'A'+i+j-2;
+            cout<<a;
+            a++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+    return 0;
+}
