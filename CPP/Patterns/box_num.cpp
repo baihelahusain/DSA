219541fc8faaa547e85f8770eb6491c89d49552d
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-15 16:34:35 +0530

    Add files via upload

---
+++ CPP/Patterns/box_num.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    int n;
+    cin>>n;
+    while(i<n){
+        int j = 1;
+        while(j<=n){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+    }
+}
---
+++ CPP/Patterns/box_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i = 1;
+    cin>>n;
+    while(i<=n){
+
+        int j = 0;
+        while(j<=n){
+            cout<<i;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+}
