78ad460fe59f92d92891514b658cb1b45828aa05
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-02-19 21:07:59 +0530

    Add files via upload

---
+++ CPP/LeetCode/prod_sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long store = 0;
+        while(x!=0){
+            long int rem = x % 10;
+            store = store*10 + rem;
+            x = x/10;
+        }
+        if(store> INT_MAX || store < INT_MIN){
+            return 0;
+        }
+        return static_cast<int>(store);
+    }
+};
