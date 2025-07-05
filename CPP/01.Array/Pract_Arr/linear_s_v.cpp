7ba61793860d4b9da6471149b306e9c8ef660c60
Baihela Abid Hussain <baihelahusain@gmail.com>
2025-07-05 21:18:16 +0530

    Create linear_s_v.cpp

---
+++ CPP/01.Array/Pract_Arr/linear_s_v.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  vector<int>v={1,2,3,4,5}
+  int key = 3;
+  int size = sizeof(v)/sizeof(v[0])
+  for(int i=0;i<size;i++){
+    if(key==v[i]){
+      cout<<"key founded"<<endl;
+    }
+    return -1
+      }
+}
+    
